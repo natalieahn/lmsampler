2aa5d9aaed3e0bba119ec2cadb32f9bd6505e84f
Natalie Ahn <natalieahn@gmail.com>
2018-04-03 10:49:00 -0700

    name change

--- example_main.py
+++ example_main.py
@@ -1,11 +1,11 @@
-# Example code for using the methods in the LMSamplr class
+# Example code for using the methods in the LMSampler class
 #
 # Natalie Ahn
 # April 2018
 
 import pandas as pd
-from lmsamplr import LMSamplr
-samplr = LMSamplr()
+from lmsampler import LMSampler
+sampler = LMSampler()
 
 data = pd.read_csv(...)
 y_var = 'outcome'
@@ -17,15 +17,15 @@ clusters_optional = ['city', 'state']
 lag_suffixes = ['_lag%dmo' % mo for mo in range(1, 13)]
 n_iter = 1000
 
-results = samplr.fit_permute(data, y_var, X_vars,
+results = sampler.fit_permute(data, y_var, X_vars,
                              controls_optional=controls_optional, fixed_use=fixed_use,
                              fixed_optional=fixed_optional, clusters_optional=clusters_optional,
                              X_suffixes=lag_suffixes, n_iter = n_iter)
 
 with open('test_results_summary.txt', 'w') as f:
-  samplr.summarize(results, f)
+  sampler.summarize(results, f)
 
-samplr.plot(results, 'test_results_figure.png')
+sampler.plot(results, 'test_results_figure.png')
 
 with open('test_results_byparams.txt', 'w') as f:
-  samplr.analyze_params(results, f, threshold=.1, max_order=2)
+  sampler.analyze_params(results, f, threshold=.1, max_order=2)
